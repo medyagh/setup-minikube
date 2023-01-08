c9506c9a9c7dee392da57d78203913a1f1602c94
Ben <ben@armosec.io>
2023-01-08 19:28:42 +0200

    Aligning with code rules

--- src/inputs.ts
+++ src/inputs.ts
@@ -20,7 +20,8 @@ export const setArgs = (args: string[]) => {
   inputs.forEach((input) => {
     // If key is feature-gates, we shound not convert to lower case
     const value = (input.key === 'feature-gates') ?
-      getInput(input.key) : getInput(input.key).toLowerCase()
+      getInput(input.key) :
+      getInput(input.key).toLowerCase()
     if (value !== '') {
       args.push(input.flag, value)
     }
