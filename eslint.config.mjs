b34b7b21207b58846bb0408eaf67312e68660c7c
Medya Gh <medya@google.com>
2025-01-22 12:53:43 -0800

    flat file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,51 +1,9 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-check
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default [{
-    files: ["src/*.ts"],
-    ignores: ["**/dist/", "**/lib/", "**/node_modules/"],
-}, ...compat.extends("google"), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
-
-        parser: tsParser,
-        ecmaVersion: 13,
-        sourceType: "module",
-    },
-
-    rules: {
-        "comma-dangle": ["error", {
-            arrays: "only-multiline",
-            objects: "only-multiline",
-            imports: "only-multiline",
-            exports: "only-multiline",
-            functions: "never",
-        }],
-
-        indent: ["error", 2, {
-            SwitchCase: 1,
-        }],
-
-        "require-jsdoc": 0,
-        semi: 0,
-    },
-}];
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
