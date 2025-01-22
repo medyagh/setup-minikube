81539d59250e0f586a2040cb4494deae8295c692
Medya Gh <medya@google.com>
2025-01-22 13:26:29 -0800

    dsiable unused

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,12 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import rules from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off', // This disables the rule completely
+  }
 );
